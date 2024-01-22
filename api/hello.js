51cae2c1274ede2a1a40cb4002e84a2a16b6f9f4
Fredrik Welander <welandfr@arcda.fi>
2024-01-22 12:58:48 +0200

    lektion 2

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,26 @@
-export default function handler(req, res) {
+import { sql } from '@vercel/postgres';
+
+export default async function handler(req, res) {
+
+    if (1 == 2 && req.method == "POST") {
+        const { name, species, age} = req.body;
+
+        await sql`INSERT INTO pets (
+                name, species, age
+            ) VALUES (
+                ${name}, ${species}, ${age}
+            )`;
+
+        res.status(200).json({status: "created new pet"});
+    }
+
+    const { rows } = await sql`SELECT * FROM pets`;
+
     res.status(200).json({
         method: req.method,
-        hello: "World!"
+        hello: "World!",
+        env: process.env.FOO,
+        pets: rows
+
     });
 }
